d1c7533b84f4a117f9c59593ddd775879564d629
ojvl1 <carlosantompietri175@gmail.com>
2024-01-19 17:58:04 -0400

    prints all possible different combinations of two digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for a in range(10):
-    for b in range(a + 1, 10):
-        if a != b:
-            print('{:d}{:d}'.format(a, b),
-                  end=', ' if a < 8 or b < 9 else '\n',
-                  flush=True)
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 != 8:
+            print('{}{}, '.format((digit1), (digit2)), end='')
+        else:
+            print('{}{}'.format((digit1), (digit2)))
