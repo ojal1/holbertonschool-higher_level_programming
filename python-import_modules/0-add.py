3e4b6cccc89a67edc42cfe68262a9a9dd4c94b2f
ojvl1 <carlosantompietri175@gmail.com>
2024-01-22 18:38:58 -0400

    fix

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,6 @@
-from add_0 import add
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
 a = 1
 b = 2
-result = a + b
-print(f"{a} + {b} = {result}\n")
+print(f"{a} + {b} = {add(a, b)}\n")
