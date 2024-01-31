950f65313ef34f674a6973c80d53316614334aab
ojvl1 <carlosantompietri175@gmail.com>
2024-01-31 14:31:48 -0400

    defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """ Create a class call Square
+    """
+
+    def __init__(self, size):
+        """ Create a private attribute
+        """
+        self.__size = size
