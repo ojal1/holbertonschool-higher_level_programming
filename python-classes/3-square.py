c0e151a63127fb898beaaad8cf9cf6c72a294fb9
ojvl1 <carlosantompietri175@gmail.com>
2024-01-31 16:37:46 -0400

    class Square that defines a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,6 +3,7 @@
     Create a class call Square
 """
 
+
 class Square:
     def __init__(self, size=0):
         """
@@ -13,7 +14,6 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
 
     def area(self):
         return self.__size ** 2
