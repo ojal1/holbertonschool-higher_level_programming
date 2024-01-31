87c40021f0f3c6f4e9369c3271454f799bd75b8c
ojvl1 <carlosantompietri175@gmail.com>
2024-01-31 15:08:46 -0400

    class Square that defines a square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,9 +3,9 @@
     Create a class call Square
 """
 
+
 class Square:
-    """
-        Create a private attribute
+    """Create a private attribute
     """
     def __init__(self, size):
         self.__size = size
