32a5bc28689c0768bee9bbfc83a848aa2ca46e44
ojvl1 <carlosantompietri175@gmail.com>
2024-01-31 21:23:06 -0400

    class Square that defines a square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -29,13 +29,14 @@ class Square:
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an interger")
-        if value <0:
+
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
         return self.__size ** 2
-    
+
     def my_print(self):
         if self.__size is 0:
             print()
