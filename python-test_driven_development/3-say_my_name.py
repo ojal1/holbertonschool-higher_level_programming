c7b6cde8433aa314990a18ceaaa31304e291afe6
ojvl1 <carlosantompietri175@gmail.com>
2024-02-02 13:52:19 -0400

    fix if last name doesn't exist

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -10,5 +10,7 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    else:
+    if last_name is not "":
         print("My name is {}{}".format(first_name, last_name))
+    else:
+        print("My name is {} {}".format(first_name, last_name))
