e6cb985aaa9b4317f68b4e1e981a4ad13fd5006e
ojvl1 <carlosantompietri175@gmail.com>
2024-02-02 13:42:13 -0400

    add the module documented

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Module: 3-say_my_name"""
 def say_my_name(first_name, last_name=""):
+    """
+    Description: Print a name
+    first_name - first name to print
+    last_name - second name to print
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
