b4e5b7137567d5318a82e9f22d7f948e7ef04e61
ojvl1 <carlosantompietri175@gmail.com>
2024-02-05 14:52:15 -0400

    empty class

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/pyhton3
-"""Module: 0-rectangule.py"""
+"""
+Module: 0-rectangule.py
+Author: Osvaldo J. Antompietri
+"""
 
 
 class Rectangle:
     """
-        Create an empty class
+    Create an empty class
     """
     pass
