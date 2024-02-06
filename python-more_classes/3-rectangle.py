660b557cc125de28c96168f39e5b0ef75160d956
ojvl1 <carlosantompietri175@gmail.com>
2024-02-06 11:53:53 -0400

    print the hashtag

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Module: 3-rectangle"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return self.__width * 2 + self.__height * 2
+
+    def __str__(self):
+        hash = ""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        for _ in range(self.__height):
+            hash += '#' * self.__width
+            hash += '\n'
+        return hash.strip()
