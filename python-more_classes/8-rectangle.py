7430fa051f0a27cde3a126dc13d1fc3467e0781e
ojvl1 <carlosantompietri175@gmail.com>
2024-02-07 15:02:59 -0400

    fix

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -56,6 +56,16 @@ class Rectangle:
         else:
             return self.__width * 2 + self.__height * 2
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
     def __str__(self):
         hash = ""
         if self.__width == 0 or self.__height == 0:
@@ -71,7 +81,6 @@ class Rectangle:
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
