738d107e8345f03c48fe1a015a2d44b443322b54
ojvl1 <carlosantompietri175@gmail.com>
2024-02-11 00:09:19 -0400

    delete white space

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -15,4 +15,4 @@ class Rectangle(BaseGeometry):
         self.integer_validator("width", width)
         self.integer_validator("heigth", height)
         self.__width = width
-        self.__height = height 
+        self.__height = height
