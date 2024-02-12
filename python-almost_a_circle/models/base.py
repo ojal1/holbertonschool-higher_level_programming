7f0e957e90a98a4e4fe3112b0ea8506f521d1299
ojvl1 <carlosantompietri175@gmail.com>
2024-02-12 19:52:16 -0400

    first class Base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Create a class call Base"""
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is None:
+            Base.__nb_objects += 1
+            id = Base.__nb_objects
+        self.id = id
