9a62aab2086facbbf64dc7ef716e070b04ba3606
ojvl1 <carlosantompietri175@gmail.com>
2024-02-13 15:00:24 -0400

    fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -42,7 +42,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """str method"""
-        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}")
+        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} 
+        - {self.width}/{self.height}")
 
     @property
     def width(self):
