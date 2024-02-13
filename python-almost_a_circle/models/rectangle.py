77453c3f02c3eed6bf841da1c15eb9f643e4b6cb
ojvl1 <carlosantompietri175@gmail.com>
2024-02-13 15:09:07 -0400

    fix pycode(line too long)

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -42,8 +42,12 @@ class Rectangle(Base):
 
     def __str__(self):
         """str method"""
-        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} 
-                - {self.width}/{self.height}")
+        id = self.id
+        x = self.x
+        y = self.y
+        width = self.width
+        height = self.height
+        return (f"[Rectangle] ({id}) {x}/{y} - {width}/{height}")
 
     @property
     def width(self):
