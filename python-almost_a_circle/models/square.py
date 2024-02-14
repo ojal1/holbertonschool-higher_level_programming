9fde9595e31d938cade614679344456aa1bdebf2
ojvl1 <carlosantompietri175@gmail.com>
2024-02-14 14:03:45 -0400

    string representation

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/pyhton3
+"""Class call Square"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(self, size, size, x, y, id)
+
+    def __str__(self):
+        """string representation"""
+        my_string = "[Square] ({}) ".format(self.id)
+        my_string += "{}/{} ".format(self.x, self.y)
+        my_string += "- {}".format(self.width)
+        return my_string
