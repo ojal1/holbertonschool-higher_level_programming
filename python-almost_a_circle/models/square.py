2f99fd82fa37f938066ed51adc2d2815eba27111
ojvl1 <carlosantompietri175@gmail.com>
2024-02-14 14:34:34 -0400

    fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Defines Square"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(self, size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """string representation"""
