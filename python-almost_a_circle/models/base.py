c64bf951ae955c9563f979ddafc6019138cb647a
ojvl1 <carlosantompietri175@gmail.com>
2024-02-15 19:29:48 -0400

    adding the class method def sae_to_file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,3 +22,16 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation"""
+        if list_objs is None:
+            list_objs = []
+        filename = f"{cls.__name__}.json" # create the file name of class in .json
+        list = [] # save the list of objects to a file
+        for obj in list_objs:
+            list.append(obj.to_dictionary())
+        with open(filename, "w") as f: # search of the file name with .json
+            json_str = cls.to_json_string(list)
+            f.write(json_str)
