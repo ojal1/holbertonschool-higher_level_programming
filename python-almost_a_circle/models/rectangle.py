15a48ba228d50495977218741b50915b1c75ce16
ojvl1 <carlosantompietri175@gmail.com>
2024-02-15 11:10:59 -0400

    add a method call to_dictionary

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -53,6 +53,15 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
+    def to_dictionary(self):
+        """Return a dictionary"""
+        return {"id": self.id, 
+                "width": self.width, 
+                "height": self.height, 
+                "x": self.x, 
+                "y": self.y
+                }
+
     def __str__(self):
         """str method"""
         id = self.id
