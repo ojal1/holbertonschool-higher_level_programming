ff21dc02b19ea72c45cbac29f10bef8ca767dfd8
ojvl1 <carlosantompietri175@gmail.com>
2024-02-15 13:01:40 -0400

    Return the JSON string representation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """Create a class call Base"""
 
 
+import json
+
 class Base:
     """define the base"""
     __nb_objects = 0
@@ -11,3 +13,11 @@ class Base:
             Base.__nb_objects += 1
             id = Base.__nb_objects
         self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation"""
+        if not list_dictionaries or list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
