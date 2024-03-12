72c4d96c48ed8db95cfddc6061246b3e3400b7f4
ojvl1 <antompietriosvaldo@gmail.com>
2024-03-12 13:49:47 -0400

    fix

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,5 @@
 -- script that creates the table unique_id
 CREATE TABLE IF NOT EXISTS unique_id(
-    id INT DEFAULT 1,
-    name VARCHAR(256),
-    UNIQUE (unique_id)
+    id INT UNIQUE DEFAULT 1,
+    name VARCHAR(256)
 );
