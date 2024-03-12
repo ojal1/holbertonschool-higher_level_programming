cd4c3d70a74743dd84a3b54bf822bf1e62c85333
ojvl1 <antompietriosvaldo@gmail.com>
2024-03-12 14:20:49 -0400

    fix

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,7 +4,9 @@
 -- name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    state_id INT NOT NULL FOREIGN KEY REFERENCES hbtn_0d_usa.states(id),
-    name VARCHAR(256) NOT NULL
+    id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 );
