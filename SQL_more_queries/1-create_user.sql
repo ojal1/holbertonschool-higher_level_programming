d5d74596f7fdb0cea8728d2fd38bcf151d6b61d4
ojvl1 <antompietriosvaldo@gmail.com>
2024-03-11 23:45:11 -0400

    fix

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@
 -- give all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd'
-GRANTS ALL PRIVILEGES ON user_0d_1;
+GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
