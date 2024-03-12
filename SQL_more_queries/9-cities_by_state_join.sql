25b6775599ae5c6d9a113cf9ccbf2ce1b2bee446
ojvl1 <antompietriosvaldo@gmail.com>
2024-03-12 14:42:19 -0400

    change a name

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -3,5 +3,5 @@
 -- Results must be sorted in ascending order by cities.id
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = state.id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
