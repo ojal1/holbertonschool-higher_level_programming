8391c5e7d74b067609ac38f4f4b4e5b61c8bf3a9
ojvl1 <antompietriosvaldo@gmail.com>
2024-03-11 23:43:40 -0400

    add the comments

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
+-- create user if exist
+-- set a password
+-- give all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd'
 GRANTS ALL PRIVILEGES ON user_0d_1;
